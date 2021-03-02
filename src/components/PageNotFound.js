ee37876ef1aa9ccca7bdb26312f9a0da5e6df917
Luiz Vicentin <luizpavanello@gmail.com>
2021-03-02 15:08:50 -0300

    Created the PageNotFound

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+  return (
+    <p>
+      Page not found. Go to <link to='/'>Home</link>
+    </p>
+  );
+};
+
+export default PageNotFound;
