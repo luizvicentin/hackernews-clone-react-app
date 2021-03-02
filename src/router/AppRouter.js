b047c66186ea2f72021d5d779c8ab056e0e00056
Luiz Vicentin <luizpavanello@gmail.com>
2021-03-02 17:37:29 -0300

    Update the Router and ShowStories | delete homepage unnecessary information

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from '../components/Header'
-import HomePage from '../components/HomePage'
-import ShowStories from '../components/ShowStories';
-import PageNotFound from '../components/PageNotFound'
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "../components/Header";
+import ShowStories from "../components/ShowStories";
+import PageNotFound from "../components/PageNotFound";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <div clasName='container'>
-      <Header />
-      <Switch>
-        <Route path='/' component={ HomePage } exact={ true }/>
-        <Route path="/:type" component={ ShowStories } />
-        <Route component={ PageNotFound } />
-      </Switch>
-    </div>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" render={() => <Redirect to="/top" />} exact={true} />
+          <Route
+            path="/:type"
+            render={({ match }) => {
+              const { type } = match.params;
+              if (!['top', 'new', 'best'].includes(type)) {
+                return <Redirect to="/" />;
+              }
+              return <ShowStories type={type} />;
+            }}
+          />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 };
